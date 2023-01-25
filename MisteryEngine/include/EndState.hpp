2973f8c51bad76ce35be1dd4f5bf1938349625dc
João Victor Cabral de Melo <joao.victor.cabralmelo@gmail.com>
2023-01-25 15:43:53 -0300

    Final State

---
+++ MisteryEngine/include/EndState.hpp
@@ -0,0 +1,29 @@
+#ifndef _END_STATE_H_
+#define _END_STATE_H_
+
+#include "State.hpp"
+#include "Music.hpp"
+
+class EndState : public State {
+  public:
+    EndState();
+
+    ~EndState();
+
+    void LoadAssets();
+
+    void Update(float dt);
+
+    void Render();
+
+    void Start();
+
+    void Pause();
+
+    void Resume();
+  
+  private:
+    Music backgroundMusic;
+};
+
+#endif 
